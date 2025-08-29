e93c7dba8d8679e218710ff4df51311ca2ab2fde
Damjan Krstic <D.Krstic.inf24@stud.bbbaden.ch>
2025-08-29 11:16:52 +0200

    Add files via upload

---
+++ RPG/Waffe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    internal class Waffe
+    {
+        public string Name { get; internal set; }
+        public int MaxDamage { get; internal set; }
+        public int MinDamage { get; internal set; }
+        public int RollDamage { get; internal set; }
+
+        internal void RollDamage(int v) => throw new NotImplementedException();
+    }
+}
